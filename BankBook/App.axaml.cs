5c83eb0eec9467e68269c0f87c6682a62df0237e
c3p1-dev <mat.clot@gmail.com>
2025-05-23 22:08:07 +0200

    Creation of TemperatureConvertor control. I moved the tutorial from the main window to a new user control "TemperatureConvertor".

--- BankBook/App.axaml.cs
+++ BankBook/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using BankBook.Windows;
 
 namespace BankBook
 {
---
+++ BankBook/Windows/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BankBook.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
