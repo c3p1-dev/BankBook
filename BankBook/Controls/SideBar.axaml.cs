8366ec7a083636a3b9f7ad08ea2a70128cc1b336
c3p1-dev <mat.clot@gmail.com>
2025-06-19 01:03:14 +0200

    Progress on UI
    Add a SideBar usercontrol
    Add Dashboard, Accounts, Classes and Subclasses icons

---
+++ BankBook/Controls/SideBar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BankBook.Controls;
+
+public partial class SideBar : UserControl
+{
+    public SideBar()
+    {
+        InitializeComponent();
+    }
+}
